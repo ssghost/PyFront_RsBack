0e72d82eda01c425a8895af3ec1f5377a61dbb85
ssghost <ssprof0@gmail.com>
2023-01-18 23:37:48 +0800

    modify model

--- frontend/model.py
+++ frontend/model.py
@@ -1,11 +1,9 @@
 from PyQt6 import QtGui
 
 DATA_FILE = "../data/res.txt"
-TASK_LIST = []
 
 with open(DATA_FILE) as f:
-    t = f.readline()
-    TASK_LIST.append(t)
+    TASK_LIST = [str(line) for line in f]
 
 def create_model() -> QtGui.QStandardItemModel:
     model = QtGui.QStandardItemModel()
