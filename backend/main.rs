50591bd97eeb83f9e039181a03e94fcbbc850251
Shuang Song <6882971+ssghost@users.noreply.github.com>
2023-01-18 07:59:14 +0000

    add rs fs

--- backend/main.rs
+++ backend/main.rs
@@ -1,4 +1,5 @@
-#![allow(unused)] // While exploring, remove for prod.
+#![allow(unused)] 
+use std::fs;
 use anyhow::{anyhow, Result};
 use std::collections::BTreeMap;
 use surrealdb::sql::{thing, Datetime, Object, Thing, Value};
